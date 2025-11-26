8c35cf11152f45d12c39ac39ba325c115e8b60e5
kanishk <101551545+iamnotkanishk@users.noreply.github.com>
2025-11-26 03:57:49 +0000

    staged commit

--- myPWA/public/app.js
+++ myPWA/public/app.js
@@ -1,5 +1,6 @@
 let result = "";
-fetch('./frontEndData.json')
+
+fetch("./frontEndData.json")
   .then(function (response) {
     return response.json();
   })
@@ -7,19 +8,23 @@ fetch('./frontEndData.json')
     appendData(data);
   })
   .catch(function (err) {
-    console.log('error: ' + err);
+    console.log("error: " + err);
   });
-  function appendData(data) {
-    data.forEach(({ name, image, hyperlink, about, language } = rows) => {
-      result += `
-       <div class="card">
-            <img class="card-image" src="${image}" alt="Product image for the ${name} VSCode extension."/>
-            <h1 class="card-name">${name}</h1>
-            <p class="card-about">${about}</p>
-            <a class="card-link" href="${hyperlink}"><button class="btn">Read More</button></a>
-        </div>
-       `;
-    });
-    document.querySelector(".container").innerHTML = result;
-  }
 
+function appendData(data) {
+  data.forEach(({ name, image, hyperlink, about, language }) => {
+    result += `
+      <div class="card">
+        <img class="card-image" src="${image}" alt="Product image for the ${name} VSCode extension."/>
+        <h1 class="card-name">${name}</h1>
+        <p class="card-about">${about}</p>
+        <a class="card-link" href="${hyperlink}">
+          <button class="btn">Read More</button>
+        </a>
+      </div>
+    `;
+  });
+
+  // Insert into the DOM (assuming you have a container element)
+  document.querySelector(".container").innerHTML = result;
+}
--- myPWA/public/serviceWorker.js
+++ myPWA/public/serviceWorker.js
@@ -1,56 +1,47 @@
 const assets = [
-    "/",
-    "css/style.css",
-    "js/app.js",
-    "/images/logo.png",
-    "/images/blog2.jpg",
-    "/images/favicon.jpg",
-    "/icons/icon-128x128.png",
-    "/icons/icon-192x192.png",
-    "/icons/icon-384x384.png",
-    "/icons/icon-512x512.png"
-  ];
+  "/",
+  "css/style.css",
+  "js/app.js",
+  "/images/logo.png",
+  "/images/blog2.jpg",
+  "/images/favicon.jpg",
+  "/icons/icon-128x128.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png"
+];
 
+const CATALOGUE_ASSETS = "catalogue-assets-v1"; // version your cache
 
-const CATALOGUE_ASSETS = "catalogue-assets";
-
-
+// Install event
 self.addEventListener("install", (installEvt) => {
   installEvt.waitUntil(
-    caches
-      .open(CATALOGUE_ASSETS)
-      .then((cache) => {
-        console.log(cache)
-        cache.addAll(assets);
-      })
-      .then(self.skipWaiting())
-      .catch((e) => {
-        console.log(e);
-      })
+    caches.open(CATALOGUE_ASSETS).then((cache) => {
+      console.log("Caching assets...");
+      return cache.addAll(assets);
+    }).then(() => self.skipWaiting())
+    .catch((e) => console.error("Install error:", e))
   );
 });
 
-
-self.addEventListener("activate", function (evt) {
+// Activate event
+self.addEventListener("activate", (evt) => {
   evt.waitUntil(
-    caches
-      .keys()
-      .then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key === CATALOGUE_ASSETS) {
-              console.log("Removed old cache from", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CATALOGUE_ASSETS) {
+            console.log("Removed old cache:", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
-
-self.addEventListener("fetch", function (evt) {
+// Fetch event
+self.addEventListener("fetch", (evt) => {
   evt.respondWith(
     fetch(evt.request).catch(() => {
       return caches.open(CATALOGUE_ASSETS).then((cache) => {
@@ -58,4 +49,4 @@ self.addEventListener("fetch", function (evt) {
       });
     })
   );
-})
+});
